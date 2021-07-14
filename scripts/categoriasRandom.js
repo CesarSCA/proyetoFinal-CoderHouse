03ee2383d5d9c5b57c9c4d03c5ea4d30455e7834
Cesar Castro <santiagotato36@gmail.com>
2021-07-13 23:46:43 -0300

    varias mejoras, buscando un mejor aspecto visual y que sea responsive, ademas de agregar mas cosas reales

--- scripts/categoriasRandom.js
+++ scripts/categoriasRandom.js
@@ -1,13 +1,16 @@
 window.addEventListener("load", function() {
     let numero = 1
-    var a = ["Cactus","Interior","Exterior","Jardin","Trepaderas","Aquaticas", "Masetas"]
+    var a = ["Accesorios", "Masetas", "Exterior", "Interior", "Suculentas"]
     var res = a.sort(function() {
     return 0.5 - Math.random();
     });
     let seleccionadas = res.slice(a[numero],4)
     for (let index = 0; index < 4; index++) {
         document.getElementById(`producto__categoria${numero}`).innerHTML+= `
-        <h2>${(seleccionadas[index])}</h2>
+        <div class="titulo__productos" style="display: flex; flex-direction: row;">
+            <h2>${(seleccionadas[index])}</h2>
+            <span class="vermas"><a href="./categorias/subcategorias/${seleccionadas[index]}.html">ver mas</a></span>
+        </div>
         <div class="contenedorIMG">
         <div class="caja__imagenes"><div id="categoria__${seleccionadas[index]}1" class="categorias__imagenes productos__imagen"></div><span>$100</span></div>
         <div class="caja__imagenes"><div id="categoria__${seleccionadas[index]}2" class="categorias__imagenes productos__imagen"></div><span>$100</span></div>
@@ -18,14 +21,33 @@ window.addEventListener("load", function() {
             for (let i = 1; i < 4; i++) {
                 document.getElementById(`categoria__Cactus${i}`).style.backgroundImage = `url(assets/categorias/Cactus/cactus${i}.jpg)`
             }
-        // } else if(seleccionadas[index] == "Masetas"){
-        //     for (let i = 1; i < 4; i++) {
-        //         let dsa = Math.floor((Math.random() * 18) + 1);
-        //         var asd = `url(assets/categorias/Masetas/masetas((${(dsa)})).jpg)`
-        //         console.log(asd)
-        //         document.getElementById(`categoria__Masetas${i}`).style.backgroundImage = asd
-        //     }
-        } else {
+        } else if(seleccionadas[index] == "Masetas"){
+            for (let i = 1; i < 4; i++) {
+                let dsa = Math.floor((Math.random() * 18) + 1);
+                document.getElementById(`categoria__Masetas${i}`).style.backgroundImage = `url(\"./assets/categorias/Masetas/masetas(${dsa}).jpg\")`
+            }
+        } else if(seleccionadas[index] == "Accesorios"){
+            for (let i = 1; i < 4; i++) {
+                let dsa = Math.floor((Math.random() * 18) + 1);
+                document.getElementById(`categoria__Accesorios${i}`).style.backgroundImage = `url(\"./assets/categorias/Accesorios/accesorios(${dsa}).webp\")`
+            }
+        } else if(seleccionadas[index] == "Exterior"){
+            for (let i = 1; i < 4; i++) {
+                let dsa = Math.floor((Math.random() * 10) + 1);
+                document.getElementById(`categoria__Exterior${i}`).style.backgroundImage = `url(\"./assets/categorias/PlantasExterior/exterior(${dsa}).jpg\")`
+            }
+        } else if(seleccionadas[index] == "Interior"){
+            for (let i = 1; i < 4; i++) {
+                let dsa = Math.floor((Math.random() * 38) + 1);
+                document.getElementById(`categoria__Interior${i}`).style.backgroundImage = `url(\"./assets/categorias/PlantasInterior/interior(${dsa}).jpg\")`
+            }
+        } else if(seleccionadas[index] == "Suculentas"){
+            for (let i = 1; i < 4; i++) {
+                let dsa = Math.floor((Math.random() * 21) + 1);
+                document.getElementById(`categoria__Suculentas${i}`).style.backgroundImage = `url(\"./assets/categorias/Suculentas/suculentas(${dsa}).jpg\")`
+            }
+        }
+        else {
             for (let i = 1; i < 4; i++) {
                 document.getElementById(`categoria__${seleccionadas[index]}${i}`).style.backgroundColor = `rgba(217, 227, 236, 0.952)`
             }
